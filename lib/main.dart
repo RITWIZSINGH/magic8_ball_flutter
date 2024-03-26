73ffe27f7e71470d07e178390c920c12bd67a3a4
Ritwiz <madangopalsingh2803@gmail.com>
2024-03-26 17:25:36 +0530

    Completed the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,61 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'Ask me Anything',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int faceno = 1;
+  ChangeBallFace() {
+    setState(() {
+      faceno = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                ChangeBallFace();
+              },
+              child: Image.asset('images/ball$faceno.png'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+              ),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
